1b8651b89adf0477d99c358e93d6eb986993f4f4
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-04 22:22:34 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -6,29 +6,13 @@ function change_css(style) {
 	//setAttribute('href', style);
 }
 
-//function change_css 2nd version (I prefere the first one)//
-function change_css2(style) {
-	var n_css;
-	if (style == 0) {
-		n_css = "assets\default.css"
-	}
-	if (style == 2) {
-		n_css = "assets/css/1800.css"
-	}
-	if (style == 4) {
-		n_css = "assets/1900-50.css"
-	}  //else{
-	    //n_css = "abcd.css"
-	  //}
-	 document.getElementById('css').href = n_css;
-}
-
+//set variables for the 3 articles in the 3 issues//
 var art1, art2, art3
 art1 = "1_1.html";
 art2 = "1_2.html";
 art3 = "1_3.html";
 
-//function load default article in the frame{//
+//function load default article in the IssuesViewer page//
 $(document).ready(function(){
 	$("#artM").load(art1);
 	$("#artL1").load(art2);
@@ -79,6 +63,7 @@ function slide_articles(){
 	}
 }
 
+//function MetaDataViewer - visualization//
 function MDV(selection){
 	let text = "";
 	if (selection == 1){
@@ -97,42 +82,15 @@ function MDV(selection){
 	document.getElementById(id).innerHTML = text;
  }
 
+//function openPopUp//
+//<a class="dropdown-item" href="https://www.unibo.it" onclick="openpopup()">label</a>"
+//function openPopUp(){
+
 //function to hide/show entities [not working]//
 //function entities(){//
 //	$(".entity").hide(); 
 //	$(".entity").show(); 
 //}
-
-//previous attempts to load issues//
-//$(document).ready(function(){
-//	$("#I1").click(function(){
-//    $(function(){
-//      $("#artM").load("1_1.html");
-//	$("#artL1").load("1_2.html");
-//	$("#artL2").load("1_3.html")
-//    });
-//});
-// })
-
-//$(document).ready(function(){
-//	$("#I2").click(function(){
-//    $(function(){
-//      $("#artM").load("2_1.html");
-//	$("#artL1").load("2_2.html");
-//	$("#artL2").load("2_3.html")
-//    });
-//});
-// })
-
-//$(document).ready(function(){
-//	$("#I3").click(function(){
-//    $(function(){
-//      $("#artM").load("3_1.html");
-//	$("#artL1").load("3_2.html");
-//	$("#artL2").load("3_3.html")
-//    });
-//});
-// })
 
 //function to high_href() [working off-line with $(document).ready(function(){ not online]//
 //function high_href(){//
