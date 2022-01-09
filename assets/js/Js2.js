4fd024addbc7919b0e202835fa14028178113600
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-09 17:25:38 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -15,12 +15,14 @@ metadata_issue = '<h6 id="main_art_title">1.1 From the Fair Theatre to the Moder
 
 //function load default article in the IssuesViewer page//
 $(document).ready(function(){
-	$("#artM").load(art1);
-	$("#artL1").load(art2);
-	$("#artL2").load(art3);
-	document.getElementById("issue_title").innerHTML = document.getElementById(issue_id).innerText;
-	document.getElementById("issue_metadata").innerHTML = metadata_issue;
-	})
+	change_issue(1)
+	}
+	//$("#artM").load(art1);
+	//$("#artL1").load(art2);
+	//$("#artL2").load(art3);
+	//document.getElementById("issue_title").innerHTML = document.getElementById(issue_id).innerText;
+	//document.getElementById("issue_metadata").innerHTML = metadata_issue;
+	//})
 
 //function change_issue//
 function change_issue(issue) {
@@ -49,7 +51,7 @@ function change_issue(issue) {
 	$("#artM").load(art1);
 	$("#artL1").load(art2);
 	$("#artL2").load(art3);
-	metadata_issue = '<h6 id="main_art_title">' + issue + '.1' + document.querySelector('div.title_art > h2') + '. Main article</h6>'
+	metadata_issue = '<h6 id="main_art_title">' + issue + '.1' + document.querySelector('div.title_art > h2').innerText + '. Main article</h6>'
 	//<p id="main_art_url"><a href="http://dx.doi.org/10.1590/2237-266093237">original version</a></p><h6 id="guest_art_title">1.2 El lugar de la diferencia en Chile:circo y transformismo. El caso del Circo Timoteo. Guest article</h6><p id="guest_art_url"><a href="https://raco.cat/index.php/DocumentsAnalisi/article/view/259380/346596">original version</a></p><h6 id="rev_art_title">1.3 The Semiotics of Clowns and Clowning: Rituals of Transgression and the Theory of Laughter. Book review</h6><p id="rev_art_url"><a href="https://europeanjournalofhumour.org/ejhr/article/view/126">original version</a></p>';
 	document.getElementById("issue_title").innerHTML = document.getElementById(issue_id).innerText;
 	document.getElementById("issue_metadata").innerHTML = metadata_issue;
