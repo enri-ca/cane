0bcd66ea1d04fb4cb372009ef5cd9393b899eedd
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-09 23:32:00 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -201,14 +201,15 @@ function closePopUp(){
 
 //function to high_href() [working off-line with $(document).ready(function(){ not online]//
 $(document).ready(function(){
-$(".bib_note").click(function(){
-	var target = $(this).attr("href");
-	var source_id = $(this).attr("id");
-	var source = document.getElementById(source_id);
-	$(target).addClass('highlighted');
-	$(target).click(function(){
-		$(target).removeClass('highlighted');
-		source.scrollIntoView();
+	$(".bib_note").click(function(){
+		var target = $(this).attr("href");
+		var source_id = $(this).attr("id");
+		var source = document.getElementById(source_id);
+		$(target).addClass('highlighted');
+		$(target).click(function(){
+			$(target).removeClass('highlighted');
+			source.scrollIntoView();
+			});
 		});
 	})
 
