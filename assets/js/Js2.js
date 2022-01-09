04cc5860e22df1f6e2eb5f8893c77087b88735b3
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-09 16:59:14 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -7,10 +7,10 @@ function change_css(style) {
 
 //set variables for the 3 articles and their metadata in the 3 issues//
 var art1, art2, art3, metadata_issue
-art1 = "1_1.html";
-art2 = "1_2.html";
-art3 = "1_3.html";
-issue_id = "I1";
+art1 = '1_1.html';
+art2 = '1_2.html';
+art3 = '1_3.html';
+issue_id = 'I1';
 metadata_issue = '<h6 id="main_art_title">1.1 From the Fair Theatre to the Modern Circus. Main article</h6><p id="main_art_url"><a href="http://dx.doi.org/10.1590/2237-266093237">original version</a></p><h6 id="guest_art_title">1.2 El lugar de la diferencia en Chile:circo y transformismo. El caso del Circo Timoteo. Guest article</h6><p id="guest_art_url"><a href="https://raco.cat/index.php/DocumentsAnalisi/article/view/259380/346596">original version</a></p><h6 id="rev_art_title">1.3 The Semiotics of Clowns and Clowning: Rituals of Transgression and the Theory of Laughter. Book review</h6><p id="rev_art_url"><a href="https://europeanjournalofhumour.org/ejhr/article/view/126">original version</a></p>';
 
 //function load default article in the IssuesViewer page//
@@ -43,9 +43,9 @@ function change_issue(issue) {
 		metadata_issue = '<h6 id="main_art_title">3.1 Building Resilience by Becoming a Circus Artist. Main article</h6><p id="main_art_url"><a href="https://doi.org/10.1093/jrs/fez091">original version</a></p><h6 id="guest_art_title">3.2	Enseigner les arts du cirque au collège : une dévolution artistique. Guest article</h6><p id="guest_art_url"><a href="https://journals.openedition.org/ejrieps/1362?lang=en">original version</a></p><h6 id="rev_art_title">3.3	The Welfare of Performing Animals. A Historical Perspective. Book review</h6><p id="rev_art_url"><a href="https://doi.org/10.3390/ani6110076">original version</a></p>';
 		}
 	issue_id = 'I' + issue;
-	art1 = issue + '_1.html";
-	art2 = issue + '_2.html";
-	art3 = issue + '_3.html";
+	art1 = issue + '_1.html';
+	art2 = issue + '_2.html';
+	art3 = issue + '_3.html';
 	$("#artM").load(art1);
 	$("#artL1").load(art2);
 	$("#artL2").load(art3);
