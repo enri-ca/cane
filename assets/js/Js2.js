025e18070ee2368c431f5529cd873532be5ba2e2
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-16 08:50:06 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -34,7 +34,7 @@ function change_issue(issue) {
 	$("#artL1").load(art2);
 	$("#artL2").load(art3);
 	document.getElementById("issue_title").innerHTML = document.getElementById(issue_id).innerText;
-	function load_MD() {
+	//function load_MD() {
 		main_art_h2 = document.getElementsByTagName("h2")[0];
 		document.getElementById("main_art_title").innerText = main_art_h2.innerHTML;
 		(document.getElementById("main_art_url").getElementsByTagName(a)[0]).setAttribute(href, main_art_h2.getAttribute('data-src'));
@@ -85,7 +85,7 @@ function change_issue(issue) {
 			//}
 	//	document.getElementById("issue_metadata").innerHTML = metadata_issue;
 	document.querySelectorAll("#main_art_title, #main_art_url").classList.add("highlighted");
-	}
+	//}
 }
 //function slide_articles() in an issue//
 counter=0
